db770e656c46d26aef8d2a5a95f6c5e9a4c2bd9b
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2025-04-08 12:10:31 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Welcome to my Data Analysis Project")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "let dive into analysis"
 )
